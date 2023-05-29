65280c0d5e59056e153e41f3695798b4e01a0d39
deeenes <turei.denes@gmail.com>
2023-05-30 01:00:20 +0200

    functions to filter `evidences` and to reconstruct interaction records from `evidences`

--- omnipath/_core/requests/interactions/__init__.py
+++ omnipath/_core/requests/interactions/__init__.py
@@ -1,6 +1,8 @@
 from omnipath._core.requests.interactions._utils import import_intercell_network
 from omnipath._core.requests.interactions._evidences import (
     filter_evidences,
+    from_evidences,
+    only_from,
     unnest_evidences,
 )
 from omnipath._core.requests.interactions._interactions import (
